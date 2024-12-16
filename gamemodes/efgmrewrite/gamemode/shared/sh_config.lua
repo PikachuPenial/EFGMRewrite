e3255dcac692836db367cbbc9a936c1575c6209a
PikachuPenial <jacksonwassermann55@icloud.com>
2024-12-15 20:24:44 -0600

    Finished recoil rebalancing on all guns
    1fdausdmho fcaiasudhmsew rynusfghswer

--- gamemodes/efgmrewrite/gamemode/shared/sh_config.lua
+++ gamemodes/efgmrewrite/gamemode/shared/sh_config.lua
@@ -21,7 +21,7 @@ flippedDebugMeleeWep = table.Flip( debugMeleeWep )
 if SERVER then
 
     -- modifiers
-    local modif = "\\nRecoilKickMult\\t1.25\\nVisualRecoilPunchMult\\t1.5\\nVisualRecoilRollMult\\t1.5\\nVisualRecoilSideMult\\t1\\nVisualRecoilUpMult\\t1\\nVisualRecoilMult\\t2\\nRecoilRandomSideMult\\t1\\nSwayMultMidAir\\t1.5\\nRecoilAutoControlMultShooting\\t0.05\\nRecoilUpMult\\t0.5\\nRecoilAutoControlMult\\t0.5 "
+    local modif = "\\nRecoilKickMult\\t0.75\\nVisualRecoilPunchMult\\t1.5\\nVisualRecoilRollMult\\t1.5\\nVisualRecoilSideMult\\t1\\nVisualRecoilUpMult\\t1\\nVisualRecoilMult\\t2\\nRecoilRandomSideMult\\t1\\nSwayMultMidAir\\t1.5\\nRecoilAutoControlMultShooting\\t0.05\\nRecoilUpMult\\t0.5\\nRecoilAutoControlMult\\t0.5 "
     RunConsoleCommand("arc9_modifiers", modif)
     RunConsoleCommand("arc9_mod_adstime", "0.75")
     RunConsoleCommand("arc9_mod_bodydamagecancel", "1")
