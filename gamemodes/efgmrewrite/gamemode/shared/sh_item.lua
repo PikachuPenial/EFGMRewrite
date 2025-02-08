5162803f7e35ca47d01f51da338dbbfca182b3c2
PortableTerraria <63077153+PortableTerraria@users.noreply.github.com>
2025-02-08 15:38:12 -0600

    Concrete changes
    - Fucked around w/ light_environment settings, I think it looks cooler but visibility might be a tiny bit worse
    - Added new raised area w/ fountain
    - New warehouse thingy
    - Changes around Hotel
    - Probably other stuff idk its been a while
    - Also I changed the way the item system works but it doesn't affect anything because nothing uses it yet D:
    -

--- gamemodes/efgmrewrite/gamemode/shared/sh_item.lua
+++ gamemodes/efgmrewrite/gamemode/shared/sh_item.lua
@@ -1,15 +1,46 @@
 
 ITEM = {}
 
-function ITEM.CreateItem( identifier, data, md )
-
-    local info = ReadJSON( identifier ) or ReadJSON( "missing_item" )
+function ITEM.CreateItem( id )
 
     local item = {}
     item.identifier = identifier
-    item.data = data or {}
-    item.md = md or {}
+    item.data = {}
+    item.md = {}
+
+    local info = EFGMITEMS[ id ]
+
+    if info.equipType == EQUIPTYPE.Gear then
+        
+        for k, v in ipairs( info.childContainers ) do
+
+            item.data[k] = {}
+            item.md[k] = {}
+
+        end
+
+    end
 
     return item
+
+end
+
+function ITEM.AddItem( container, childContainerID, item, posX, posY, isRotated )
+
+    // should probably be checks here for whether the item can fit or not
+
+    local index = table.insert( container.data[childContainerID], item )
+    container.md[childContainerID][index] = {
+        ["x"] = posX,
+        ["y"] = posY,
+        ["r"] = isRotated
+    }
+
+end
+
+function ITEM.RemoveItemAtIndex( container, childContainerID, index )
+
+    table.remove( container.data[childContainerID], index)
+    table.remove( container.md[childContainerID], index)
 
 end
