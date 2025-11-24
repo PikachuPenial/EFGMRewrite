a8e972f85c17fd293f9e50a905f21cdb4f76397a
PortableTerraria <63077153+PortableTerraria@users.noreply.github.com>
2025-11-24 01:15:37 -0600

    new filing cabinet container
    looks like shit and has no collision but ill fix those sometime that isnt 1:15 in the morning

--- gamemodes/efgmrewrite/entities/entities/efgm_container/shared.lua
+++ gamemodes/efgmrewrite/entities/entities/efgm_container/shared.lua
@@ -5,7 +5,8 @@ ENT.Category = "EFGM"
 ENT.BaseHealth = 10
 ENT.Limit = 999
 ENT.Owner = nil
-ENT.Model = "models/props_junk/TrafficCone001a.mdl"
+-- ENT.Model = "models/props_junk/TrafficCone001a.mdl"
+ENT.Model = "models/efgm/loot_containers/filing_cabinet.mdl"
 
 ENT.PrintName = "Container"
 ENT.Author = "Penial"
