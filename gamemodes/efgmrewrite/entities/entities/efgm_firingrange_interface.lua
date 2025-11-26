60d8390d96d36c75373965f1d5eed2fe2d7f9036
PikachuPenial <62630906+PikachuPenial@users.noreply.github.com>
2025-11-25 23:48:19 -0600

    updated concrete/customs map info
    will do firing range when awake

---
+++ gamemodes/efgmrewrite/entities/entities/efgm_firingrange_interface.lua
@@ -0,0 +1,15 @@
+
+ENT.Type = "point"
+ENT.Base = "base_point"
+
+function ENT:AcceptInput(name, ply, caller, data)
+
+    if name == "SetPlayerInRange" then
+
+        local enteredRange = tobool(data)
+
+        -- i wanna take a nap
+
+    end
+
+end
