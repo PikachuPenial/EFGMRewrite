9140b328203190143b6243ebd68176b9fe72eb91
PikachuPenial <62630906+PikachuPenial@users.noreply.github.com>
2025-12-13 11:11:40 -0600

    concrete changes and fixes
    oiufhaje

---
+++ gamemodes/efgmrewrite/entities/entities/efgm_duel_interface.lua
@@ -0,0 +1,19 @@
+
+ENT.Type = "point"
+ENT.Base = "base_point"
+
+function ENT:AcceptInput(name, ply, caller, data)
+
+    if name == "DuelBegan" then
+
+
+
+    end
+
+    if name == "DuelEnded" then
+
+
+
+    end
+
+end
